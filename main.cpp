7b3ff578d609e087e90b97ef03523f578bb9ce29
Pavan Nayak <b24bb1020@iitj.ac.in>
2026-04-17 23:05:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+/* ================= STRUCTURES ================= */
+
+struct Student {
+    string id,name,email,password;
+    int year,semester,height;
+    Student *left,*right;
+};
+
+struct Teacher {
+    string id,name,password;
+    int height;
+    Teacher *left,*right;
+};
+
+struct Enrollment {
+    string studentId;
+    double marks;
+    int height;
+    Enrollment *left,*right;
+};
+
+struct Subject {
+    string id,name,teacherId;
+    int year,semester;
+    Enrollment* root;
+    Subject* next;
+};
